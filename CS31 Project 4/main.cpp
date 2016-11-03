d3a7e529eda9d513e5266413a7bbf34698d46a71
Austin Guo <austinguo550@gmail.com>
2016-11-02 17:27:46 -0700

    Initial Commit

---
+++ CS31 Project 4/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  CS31 Project 4
+//
+//  Created by Austin Guo on 11/2/16.
+//  Copyright © 2016 Austin Guo. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
